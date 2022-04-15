953fc52ecde6e323188d7c365f4c00a157dd5fa2
Ph4nt0m <53800198+Dave-ss@users.noreply.github.com>
2022-04-15 17:32:24 -0300

    Add files via upload

---
+++ ex1/detran/Carro.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ex1.detran;
+
+/**
+ *
+ * @author davi2
+ */
+public class Carro {
+    private String condicao_geral;
+    private String documento;
+    private String placa;
+    private String nome_dono;
+    
+    public Carro(){
+    
+    }
+    
+    public void emplacamento(String placa){
+    
+    }
+    
+    public void vistoria(String condicao_geral, String nome_dono, String documento){
+    
+    }
+   
+}
---
+++ ex1/revendedora/Carro.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ex1.revendedora;
+
+/**
+ *
+ * @author davi2
+ */
+public class Carro {
+    private int tanque_combustivel;
+    private int ano_fabricacao;
+    private int quilometragem;
+    private float valor_fipe;
+    private String condicao_geral;
+    private String modelo;
+    private String marca;
+    private String placa;
+    private String nome_dono;
+    private String novo_dono;
+        
+    public Carro(){
+        
+    }
+    
+    public void venda(String modelo, String marca, String ano_fabricacao, int quilometragem, float valor_fipe){
+        
+    }
+    
+    public void muda_dono(String nome_dono, String novo_dono){
+        
+    }
+    
+    public void propaganda(int ano_fabricacao, int tanque_combustivel, String marca, String modelo){
+        
+    }
+}
