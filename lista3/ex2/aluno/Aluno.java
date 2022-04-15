f72d72011f3ba0fe8f27a7c390d7810c1261ea3d
Ph4nt0m <53800198+Dave-ss@users.noreply.github.com>
2022-04-15 17:48:46 -0300

    Add files via upload

---
+++ lista3/ex2/aluno/Aluno.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ex2.aluno;
+
+/**
+ *
+ * @author davi2
+ */
+public class Aluno {
+    private int id;
+    private int ano_nasc;
+    private String nome;
+
+    public Aluno(){
+
+    }
+    
+    public void criaAluno(String nome, int id, int ano_nasc){
+
+    }
+    
+}
---
+++ lista3/ex2/professor/Professor.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ex2.professor;
+
+/**
+ *
+ * @author davi2
+ */
+public class Professor {
+    private int matricula;
+    private int nome;
+    private float salario;
+    private int qtdTurmas;
+
+    public Professor(){
+
+    }
+    
+
+    public void criaProfessor(String nome, int matricula){
+
+    }
+
+    public void setSalario(float salario){
+        
+    }
+
+    public void setMatricula(int matricula){
+
+    }
+    
+   
+}
