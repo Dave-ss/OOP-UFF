e346421f2262fd66f3823ba572c9baff3523882e
Ph4nt0m <53800198+Dave-ss@users.noreply.github.com>
2022-04-17 16:21:22 -0300

    Update Ex4.java

--- lista3/ex4/Ex4.java
+++ lista3/ex4/Ex4.java
@@ -16,12 +16,24 @@ public class Ex4 {
         d0.criaData(01, 10, 1998);
         System.out.printf("%d/%d/%d\n", d0.getDia(),d0.getMes(),d0.getAno());
         
+        Data d01 = new Data();
+        d01.criaData(06, 7, 1911);
+        System.out.printf("%d/%d/%d\n", d01.getDia(),d01.getMes(),d01.getAno());
+        
         Data d1 = new Data();
         d1.criaData("december", 07, 1998);
         System.out.printf("%s %d, %d\n", d1.getMess(), d1.getDia(), d1.getAno());
         
+        Data d11 = new Data();
+        d11.criaData("january", 15, 1986);
+        System.out.printf("%s %d, %d\n", d11.getMess(), d11.getDia(), d11.getAno());
+        
         Data d2 = new Data();
         d2.criaData(7, 1999);
         System.out.printf("%d %d\n", d2.getDia(), d2.getAno());
+        
+        Data d21 = new Data();
+        d21.criaData(2, 1995);
+        System.out.printf("%d %d\n", d21.getDia(), d21.getAno());
     }
 }
