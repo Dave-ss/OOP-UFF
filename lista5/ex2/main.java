ffb9e653f7d367c58c6e52de5a1e1f0d7ff2a20d
Ph4nt0m <53800198+Dave-ss@users.noreply.github.com>
2022-04-25 12:46:04 -0300

    Create main.java

---
+++ lista5/ex2/main.java
@@ -0,0 +1,13 @@
+
+public class Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ConversaoDeUnidadesDeArea conversor = new ConversaoDeUnidadesDeArea();
+        conversor.printaMedidas(8250.0);
+    }
+    
+}
