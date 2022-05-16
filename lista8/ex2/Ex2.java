baeb676b09347802eb612c741333426277144e7d
Ph4nt0m <53800198+Dave-ss@users.noreply.github.com>
2022-05-16 12:42:52 -0300

    Update Ex2.java

--- lista8/ex2/Ex2.java
+++ lista8/ex2/Ex2.java
@@ -5,6 +5,7 @@
 
 package com.mycompany.ex2;
 
+import java.util.ArrayList;
 
 /**
  *
@@ -13,7 +14,24 @@ package com.mycompany.ex2;
 public class Ex2 {
 
     public static void main(String[] args) {
-        Construcao c0 = new Construcao(10, 10, true, true);
-        Carro carro = new Carro();
+        Construcao contrucao0 = new Construcao(10, 10, true, true);
+        Construcao contrucao1 = new Construcao(100, 1000, true, false);
+        Construcao contrucao2 = new Construcao(1000, 100, false, true);
+        Carro carro0 = new Carro(100, (float)1.5);
+        Carro carro1 = new Carro(1000, (float)1);
+        Carro carro2 = new Carro(10000, (float)3.0);
+        
+        ArrayList <CarbonFootPrint> array = new ArrayList<>();
+        
+        array.add(contrucao0);
+        array.add(contrucao1);
+        array.add(contrucao2);
+        array.add(carro0);
+        array.add(carro1);
+        array.add(carro2);
+        
+        for(CarbonFootPrint c : array){
+            System.out.println(c.getCarbonFootPrint());
+        }
     }
 }
